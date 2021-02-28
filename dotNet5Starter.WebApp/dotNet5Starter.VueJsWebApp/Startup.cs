422bab5a08fa3a25940f0bf27ebf3aa82ef77379
Claudiu Bruma <claudiu.bruma@gmail.com>
2021-02-28 17:47:23 +0100

    updated nuget packages

--- dotNet5Starter.WebApp/dotNet5Starter.VueJsWebApp/Startup.cs
+++ dotNet5Starter.WebApp/dotNet5Starter.VueJsWebApp/Startup.cs
@@ -1,8 +1,6 @@
 using dotNet5Starter.VueJsWebApp.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
